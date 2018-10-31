a534c3a4db6e28be0229a63a4516aa0874f5a104
TheD00mslayer <jmasc03@gmail.com>
2018-10-31 16:27:32 +0100

    rev_1.1

--- src/client.java
+++ src/client.java
@@ -1,33 +1,174 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.Socket;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class client {
+public class client extends JFrame {
+    private JPanel mainFrame;
+    private JTextArea log_client;
+    private JTextField field_ip;
+    private JTextField field_puerto;
+    private JTextField field_nick;
+    private JButton desconectarseButton;
+    private JButton conectarseButton;
+    private JButton enviarButton;
+    private JTextField field_mensaje;
+
+    private String username, address = "localhost";
+    private ArrayList users = new ArrayList();
+    private int port = 2222;
+    private Boolean isConnected = false;
+
+    private Socket sock;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+
     public static void main(String[] args){
+        client client_gui_form = new client();
+        client_gui_form.setVisible(true);
 
-        final String host = "127.0.0.1";
-        final int port = 2222;
+    }
 
-        DataInputStream in;
-        DataOutputStream out;
+    private client() {
 
-        try {
-            Socket socket = new Socket(host,port);
+        add(mainFrame);
+        setTitle("myCypher_client");
+        setSize(700, 500);
 
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
 
-            out.writeUTF("Hola mundo desde el cliente!");
+        enviarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nothing = "";
+                if ((field_mensaje.getText()).equals(nothing)) {
+                    field_mensaje.setText("");
+                    field_mensaje.requestFocus();
+                } else {
+                    try {
+                        writer.println(username + ":" + field_mensaje.getText() + ":" + "Chat");
+                        writer.flush(); // flushes the buffer
+                    } catch (Exception ex) {
+                        log_client.append("Message was not sent. \n");
+                    }
+                    field_mensaje.setText("");
+                    field_mensaje.requestFocus();
+                }
 
-            String message = in.readUTF();
+                field_mensaje.setText("");
+                field_mensaje.requestFocus();
+            }
+        });
+        conectarseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!isConnected) {
+                    username = field_nick.getText();
+                    field_nick.setEditable(false);
 
-            System.out.println(message);
+                    try {
+                        sock = new Socket(address, port);
+                        InputStreamReader streamreader = new InputStreamReader(sock.getInputStream());
+                        reader = new BufferedReader(streamreader);
+                        writer = new PrintWriter(sock.getOutputStream());
+                        writer.println(username + ":has connected.:Connect");
+                        writer.flush();
+                        isConnected = true;
+                    } catch (Exception ex) {
+                        log_client.append("Cannot Connect! Try Again. \n");
+                        field_nick.setEditable(true);
+                    }
 
-            socket.close();
+                    ListenThread();
 
-        } catch (IOException e) {
-            e.printStackTrace();
+                } else if (isConnected) {
+                    log_client.append("You are already connected. \n");
+                }
+            }
+        });
+        desconectarseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendDisconnect();
+                Disconnect();
+            }
+        });
+    }
+
+    public class IncomingReader implements Runnable {
+        @Override
+        public void run() {
+            String[] data;
+            String stream, done = "Done", connect = "Connect", disconnect = "Disconnect", chat = "Chat";
+
+            try {
+                while ((stream = reader.readLine()) != null) {
+                    data = stream.split(":");
+
+                    if (data[2].equals(chat)) {
+                        log_client.append(data[0] + ": " + data[1] + "\n");
+                        log_client.setCaretPosition(log_client.getDocument().getLength());
+                    } else if (data[2].equals(connect)) {
+                        log_client.removeAll();
+                        userAdd(data[0]);
+                    } else if (data[2].equals(disconnect)) {
+                        userRemove(data[0]);
+                    } else if (data[2].equals(done)) {
+                        //users.setText("");
+                        writeUsers();
+                        users.clear();
+                    }
+                }
+            } catch (Exception ignored) {
+            }
         }
     }
+
+    private void ListenThread() {
+        Thread IncomingReader = new Thread(new IncomingReader());
+        IncomingReader.start();
+    }
+
+    //--------------------------//
+
+    private void userAdd(String data) {
+        users.add(data);
+    }
+
+    private void userRemove(String data) {
+        log_client.append(data + " is now offline.\n");
+    }
+
+    private void writeUsers() {
+        String[] tempList = new String[(users.size())];
+        users.toArray(tempList);
+        for (String token : tempList) {
+            //users.append(token + "\n");
+        }
+    }
+
+    private void sendDisconnect() {
+        String bye = (username + ": :Disconnect");
+        try {
+            writer.println(bye);
+            writer.flush();
+        } catch (Exception e) {
+            log_client.append("Could not send Disconnect message.\n");
+        }
+    }
+
+    private void Disconnect() {
+        try {
+            log_client.append("Disconnected.\n");
+            sock.close();
+        } catch (Exception ex) {
+            log_client.append("Failed to disconnect. \n");
+        }
+        isConnected = false;
+        field_nick.setEditable(true);
+
+    }
+
 }
--- src/server.java
+++ src/server.java
@@ -1,44 +1,187 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import javax.swing.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class server {
+
+public class server extends JFrame {
+    private JTextArea log_server;
+    private JPanel mainFrame;
+    private JButton iniciarButton;
+    private JButton usuariosOnlineButton;
+    private JButton limpiarLogButton;
+    private JButton finalizarButton;
+
+
+    private ArrayList clientOutputStreams;
+    private ArrayList<String> users;
+
     public static void main(String[] args){
-        ServerSocket server;
-        Socket socket;
-        DataInputStream in;
-        DataOutputStream out;
+        server server_gui_form = new server();
+        server_gui_form.setVisible(true);
+    }
 
-        final int port = 2222;
+    private server() {
+
+        add(mainFrame);
+        setTitle("myCypher_server");
+        setSize(400, 500);
 
 
-        try {
-            server = new ServerSocket(port);
-            System.out.println("Servidor iniciado");
+        iniciarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Thread starter = new Thread(new ServerStart());
+                starter.start();
 
-            do {
-                socket = server.accept();
+                log_server.append("Server started...\n");
+            }
+        });
+        finalizarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        usuariosOnlineButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                log_server.append("\n Online users : \n");
+                for (String current_user : users) {
+                    log_server.append(current_user);
+                    log_server.append("\n");
+                }
+            }
+        });
+        limpiarLogButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                log_server.setText("");
+            }
+        });
+    }
 
-                System.out.println("Cliente conectado");
-                in = new DataInputStream(socket.getInputStream());
-                out = new DataOutputStream(socket.getOutputStream());
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket sock;
+        PrintWriter client;
 
-                String message = in.readUTF();
+        private ClientHandler(Socket clientSocket, PrintWriter user) {
+            client = user;
+            try {
+                sock = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(isReader);
+            } catch (Exception ex) {
+                log_server.append("Unexpected error... \n");
+            }
 
-                System.out.println(message);
-
-                out.writeUTF("Hola mundo desde el servidor!");
-
-                socket.close();
-
-                System.out.println("Cliente desconectado");
-            } while (true);
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
+        @Override
+        public void run() {
+            String message, connect = "Connect", disconnect = "Disconnect", chat = "Chat";
+            String[] data;
+
+            try {
+                while ((message = reader.readLine()) != null) {
+                    log_server.append("Received: " + message + "\n");
+                    data = message.split(":");
+
+                    for (String token : data) {
+                        log_server.append(token + "\n");
+                    }
+
+                    if (data[2].equals(connect)) {
+                        tellEveryone((data[0] + ":" + data[1] + ":" + chat));
+                        userAdd(data[0]);
+                    } else if (data[2].equals(disconnect)) {
+                        tellEveryone((data[0] + ":has disconnected." + ":" + chat));
+                        userRemove(data[0]);
+                    } else if (data[2].equals(chat)) {
+                        tellEveryone(message);
+                    } else {
+                        log_server.append("No Conditions were met. \n");
+                    }
+                }
+            } catch (Exception ex) {
+                log_server.append("Lost a connection. \n");
+                ex.printStackTrace();
+                clientOutputStreams.remove(client);
+            }
+        }
     }
+
+    public class ServerStart implements Runnable {
+        @Override
+        public void run() {
+            clientOutputStreams = new ArrayList();
+            users = new ArrayList();
+
+            try {
+                ServerSocket serverSock = new ServerSocket(2222);
+
+                while (true) {
+                    Socket clientSock = serverSock.accept();
+                    PrintWriter writer = new PrintWriter(clientSock.getOutputStream());
+                    clientOutputStreams.add(writer);
+
+                    Thread listener = new Thread(new ClientHandler(clientSock, writer));
+                    listener.start();
+                    log_server.append("Got a connection. \n");
+                }
+            } catch (Exception ex) {
+                log_server.append("Error making a connection. \n");
+            }
+        }
+    }
+
+
+    private void userAdd(String data) {
+        String message, add = ": :Connect", done = "Server: :Done";
+        log_server.append("Before " + data + " added. \n");
+        users.add(data);
+        log_server.append("After " + data + " added. \n");
+        String[] tempList = new String[(users.size())];
+        users.toArray(tempList);
+
+        for (String token : tempList) {
+            message = (token + add);
+            tellEveryone(message);
+        }
+        tellEveryone(done);
+    }
+
+    private void userRemove(String data) {
+        String message, add = ": :Connect", done = "Server: :Done";
+        users.remove(data);
+        String[] tempList = new String[(users.size())];
+        users.toArray(tempList);
+
+        for (String token : tempList) {
+            message = (token + add);
+            tellEveryone(message);
+        }
+        tellEveryone(done);
+    }
+
+    private void tellEveryone(String message) {
+
+        for (Object clientOutputStream : clientOutputStreams) {
+            try {
+                PrintWriter writer = (PrintWriter) clientOutputStream;
+                writer.println(message);
+                log_server.append("Sending: " + message + "\n");
+                writer.flush();
+                log_server.setCaretPosition(log_server.getDocument().getLength());
+
+            } catch (Exception ex) {
+                log_server.append("Error telling everyone. \n");
+            }
+        }
+    }
+
+
 }
